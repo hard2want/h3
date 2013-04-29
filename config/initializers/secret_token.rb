06ad83dc8d378a4c7e7a83846849c482c22e7144
Hard2Want <hard2want@hotmail.com>
2013-04-29 16:40:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+H3::Application.config.secret_token = '8a5a931c18efc89c9cbcc8529e560ee5651bae39ca5a0a4c58826b1dbf02320daaf6b8cf2d25dcb3afcd62af2136be1baf76d0eff868bd2e220bc97bdb931260'
