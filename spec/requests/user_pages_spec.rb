6c1f0e7502a47ad1e29af21a1e29a8459281eb78
Hard2Want <hard2want@hotmail.com>
2013-05-05 18:54:51 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,13 @@ subject { page }
       end
     end
 
+    describe "after submission" do
+      before { click_button submit }
+
+      # it { should have_selector('title', text: 'Sign up') }
+      it { should have_content('error') }
+    end
+
     describe "with valid information" do
       before do
         fill_in "First name",     with: "Tucker"
@@ -40,6 +47,15 @@ subject { page }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        # it { should have_selector('title', text: user.first_name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
