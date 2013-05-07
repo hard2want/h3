94ff88ef792175b11876af80b4e29c10bd6a124e
Hard2Want <hard2want@hotmail.com>
2013-05-07 11:03:38 -0500

    Github attempt

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -58,5 +58,40 @@ subject { page }
         it { should have_link('Sign out') }
       end
     end
+ # end
+
+    describe "edit" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        sign_in user
+        visit edit_user_path(user)
+      end
+
+      describe "page" do
+        it { should have_selector('h1', text: "Update your profile") }
+        it { should have_selector('title', text: "Edit user") }
+        it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      let (:new_first_name) { "New First Name" }
+      let (:new_last_name) { "New Last Name" }
+      let (:new_email) { "new@example.com" }
+      before do
+        fill_in "First name",     with: new_first_name
+        fill_in "Last name",     with: new_last_name
+        fill_in "Email",     with: new_email
+        fill_in "Password",     with: user.password
+        fill_in "Confirmation",     with: user.password
+        click_button "Save changes"
+      end
+      # it { should have_selector('title', text: user.first_name) }
+        it { should have_selector('div.alert.alert-success') }
+        it { should have_link('Sign out', href: signout_path) }
+        specify { user.reload.first_name.should == new_first_name }
+        specify { user.reload.last_name.should == new_last_name }
+        specify { user.reload.email.should == new_email }
+      end
+    end
   end
 end
